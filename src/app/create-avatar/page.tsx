ef91415f7a3d243eb84f079b900ee2de6016de47
MartelAxe <martelaxe@gmail.com>
2023-10-13 10:30:04 -0600

    fixing public key gen

--- src/app/create-avatar/page.tsx
+++ src/app/create-avatar/page.tsx
@@ -5,79 +5,80 @@ import personalSign from '../utils/signing';
 
 
 const CreateAvatar = () => {
-    const [signature, setSignature] = useState<string | null>(null);
-    const [coinbase, setCoinbase] = useState<string | null>(null);
-    const [pubkey, setPubKey] = useState<string | null>(null);
+  const [signature, setSignature] = useState<string | null>(null);
+  const [coinbase, setCoinbase] = useState<string | null>(null);
+  const [pubkey, setPubKey] = useState<string | null>(null);
 
-    const [proofRes, setProofRes] = useState<string | null>(null);
+  const [proofRes, setProofRes] = useState<string | null>(null);
 
-    const [twitter_hanlder, setTwitter] = useState<string | null>(null);
-
-
-
-    const API_URL = 'https://proof-service.nextnext.id'
-
-    const handleSignMessage = async () => {
-        const message = `{"action":"create","created_at":"1656843378","identity":"${twitter_hanlder}","platform":"twitter","prev":"KNyNFtvhlRVJh/oU6RryK2n+C2dja9aLQPjlv5VHMsQErZROojEmMAgmeEQVC094EOuHIYcv3lCYXf8d3zqDCQE=","uuid":"353449e6-3a6f-4ac8-ae65-ba14bf466baf"}`;
+  const [twitter_hanlder, setTwitter] = useState<string | null>(null);
 
 
-        const signingKey =''; //TODO: Create signinig  key with metamask
 
-        const obj = await personalSign(message, signingKey);
-        if (obj) {
-            // Remove the '0x' prefix
-            const hexSignature = obj.signature.slice(2);
+  const API_URL = 'https://proof-service.nextnext.id'
 
-            // Convert hex to base64
-            const base64Signature = Buffer.from(hexSignature, 'hex').toString('base64');
-            console.log(`Signature: ${base64Signature}`);
-            setSignature(base64Signature);
-            setCoinbase(obj.coinbase);
-            setPubKey(obj.publicKey);
-        }
+  const handleSignMessage = async () => {
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const message = '{"action":"create","created_at":"1697214332","identity":"fasdfasd","platform":"twitter","prev":null,"uuid":"430b263a-e49e-4e42-bb30-ac173da4c739"}'
+
+
+
+    const obj = await personalSign(message);
+    if (obj) {
+      // Remove the '0x' prefix
+      const hexSignature = obj.signature.slice(2);
+
+      // Convert hex to base64
+      const base64Signature = Buffer.from(hexSignature, 'hex').toString('base64');
+      console.log(`Signature: ${base64Signature}`);
+      setSignature(base64Signature);
+      setCoinbase(obj.coinbase);
+      setPubKey(obj.publicKey);
+    }
+  };
+
+  const verifyPost = async () => {
+    const body = {
+      action: "create",
+      platform: "twitter",
+      identity: twitter_hanlder,
+      public_key: pubkey!.slice(2)
     };
+    console.log(body)
+    const response = await fetch(`${API_URL}/v1/proof`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body)
+    });
+    console.log(response);
+    // {"message":"invalid secp256k1 public key: invalid secp256k1 public key: invalid secp256k1 public key: invalid secp256k1 public key"}
+    setProofRes(await response.text());
+  };
+  return (
+    <div>
+      <label>Twitter Account</label>
+      <input type="text" onChange={(e) => { setTwitter(e.target.value) }} />
+      <button onClick={handleSignMessage}>Sign Message with MetaMask</button>
+      {
+        signature &&
+        <>
+          <h3>Make post on twitter:</h3>
+          <p>🎭 Verifying my Twitter ID @your_twitter_handle for @NextDotID.</p>
+          <p>Sig: {signature}</p>
+          <br />
+          <p>Next.ID YOUR DIGITAL IDENTITIES IN ONE PLACE</p>
+          <br /><br />
+          <button onClick={verifyPost}>Verify Post</button>
+          {proofRes}
+        </>
+      }
 
-    const verifyPost = async() => {
-      const body = {
-        action: "create",
-        platform: "twitter",
-        identity: twitter_hanlder,
-        public_key: pubkey.slice(2)
-      };
-      console.log(body)
-      const response = await fetch(`${API_URL}/v1/proof`, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body)
-      });
-      console.log(response);
-      // {"message":"invalid secp256k1 public key: invalid secp256k1 public key: invalid secp256k1 public key: invalid secp256k1 public key"}
-      setProofRes(await response.text());
-    };
-    return (
-        <div>
-            <label>Twitter Account</label>
-            <input type="text" onChange={(e) => {setTwitter(e.target.value)}} />
-            <button onClick={handleSignMessage}>Sign Message with MetaMask</button>
-            {
-              signature &&
-              <>
-              <h3>Make post on twitter:</h3>
-              <p>🎭 Verifying my Twitter ID @your_twitter_handle for @NextDotID.</p>
-              <p>Sig: {signature}</p>
-              <br/>
-              <p>Next.ID YOUR DIGITAL IDENTITIES IN ONE PLACE</p>
-              <br/><br/>
-              <button onClick={verifyPost}>Verify Post</button>
-              {proofRes}
-              </>
-            }
-
-        </div>
-    );
+    </div>
+  );
 };
 
 export default CreateAvatar;
--- src/app/utils/signing.ts
+++ src/app/utils/signing.ts
@@ -2,6 +2,7 @@ import { ethers } from 'ethers';
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import * as ethUtil from 'ethereumjs-util';
+import * as secp256k1 from 'secp256k1';
 
 
 const providerOptions = {
@@ -14,7 +15,7 @@ const providerOptions = {
   walletconnect: {
     package: WalletConnectProvider, // required
     options: {
-      rpc:{
+      rpc: {
         100: "https://rpc.gnosischain.com/",
         80001: "https://rpc-mumbai.maticvigil.com"
       }
@@ -29,28 +30,53 @@ const web3Modal = new Web3Modal({
   providerOptions
 });
 
-async function personalSign(message: string, privateKey: string | ethers.SigningKey) {
-    //const wallet = new ethers.Wallet(privateKey);
-    const conn = await web3Modal.connect();
-    const provider = new ethers.BrowserProvider(conn,"any");
-    const signer = await provider.getSigner();
-    const coinbase = await signer.getAddress();
-    const messageHash = ethers.hashMessage(message);
-    const signature = await signer.signMessage(ethers.toBeArray(messageHash));
+async function personalSign(message: string) {
+  const conn = await web3Modal.connect();
+  const provider = new ethers.BrowserProvider(conn, "any");
+  const signer = await provider.getSigner();
+  const messageHash = ethers.hashMessage(message);
+  const signature = await signer.signMessage(message);
 
-     // Recover the public key
-     const msgHashBytes = Buffer.from(messageHash.slice(2), 'hex');  // Convert hex to Buffer
-     const signatureParams = ethUtil.fromRpcSig(signature);
-     const publicKey = ethUtil.ecrecover(
-       msgHashBytes,
-       signatureParams.v,
-       signatureParams.r,
-       signatureParams.s
-     );
-    return({
-      signature: signature,
-      coinbase: coinbase,
-      publicKey: ethUtil.bufferToHex(publicKey)  // Convert the public key to hex string
-    });
+  console.log('Message Hash:', messageHash);
+  console.log('Signature:', signature);
+
+  // Convert hex signature to Uint8Array
+  const signatureMatch = signature.slice(2).match(/.{1,2}/g);
+  if (!signatureMatch) {
+    throw new Error('Invalid signature format');
+  }
+  const signatureBytes = new Uint8Array(signatureMatch.map(byte => parseInt(byte, 16)));
+
+  // Ensure signatureBytes is 64 bytes long
+  if (signatureBytes.length !== 65) {
+    throw new Error('Invalid signature length');
+  }
+
+  // Convert message hash to Uint8Array
+  const messageHashMatch = messageHash.slice(2).match(/.{1,2}/g);
+  if (!messageHashMatch) {
+    throw new Error('Invalid message hash format');
+  }
+  const messageHashBytes = new Uint8Array(messageHashMatch.map(byte => parseInt(byte, 16)));
+
+  // Recover the public key
+  const recoveredPublicKey = secp256k1.ecdsaRecover(
+    signatureBytes.slice(0, 64), // Exclude the recovery id
+    signatureBytes[64] - 27, // Recovery id
+    messageHashBytes,
+    true
+  );
+
+  const publicKeyHex = '04' + Buffer.from(recoveredPublicKey).toString('hex');
+
+  console.log('Recovered Public Key:', publicKeyHex);
+
+  return {
+    signature: signature,
+    coinbase: await signer.getAddress(),
+    publicKey: publicKeyHex
+  };
 }
+
+
 export default personalSign;
